f9a960830581788309e86e46bd1657ce283475f3
Илья Уткин <ilyautkin@mail.ru>
2016-07-01 15:11:38 +0300

    Добавлено обновление страницы при сохранении
    При нажатии кнопки «Сохранить», произойдет обновление страницы. Всплывающее окно редактора окажется закрытым, а все изменения будут применены и отображены на странице

--- core/components/frontendmanager/elements/plugins/plugin.frontendmanager.php
+++ core/components/frontendmanager/elements/plugins/plugin.frontendmanager.php
@@ -1,5 +1,27 @@
 <?php
-if($modx->event->name != 'OnLoadWebDocument' || !$modx->user->isAuthenticated('mgr')) return;
-$frontendManager = $modx->getService('frontendmanager','frontendManager', MODX_CORE_PATH . 'components/frontendmanager/model/frontendmanager/', array());
-if(!$frontendManager) die('error load frontendmanager');
-$frontendManager->initialize($modx->context->key);
+if(!$modx->user->isAuthenticated('mgr')) return;
+switch ($modx->event->name) {
+    case 'OnLoadWebDocument':
+        $frontendManager = $modx->getService('frontendmanager','frontendManager', MODX_CORE_PATH . 'components/frontendmanager/model/frontendmanager/', array());
+        if(!$frontendManager) die('error load frontendmanager');
+        $frontendManager->initialize($modx->context->key);
+        break;
+    case 'OnBeforeManagerPageInit':
+        if ($_GET['frame']) {
+            $modx->controller->addHtml('<script>Ext.onReady(function() {
+                setTimeout(function() {
+                    if (Ext.getCmp("modx-panel-resource")) {
+                        Ext.getCmp("modx-panel-resource").on("success", function(){top.window.location.href = top.window.location.href});
+                    }
+                    if (Ext.getCmp("modx-panel-chunk")) {
+                        Ext.getCmp("modx-panel-chunk").on("success", function(){top.window.location.href = top.window.location.href});
+                    }
+                }, 1000);
+            });
+            </script>');
+        }
+        break;
+    default:
+        break;
+}
+return;
